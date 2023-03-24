6f48b3d2a5a059819a68c52682bbabf3c8157fd4
yanjingwen12 <59303669+yanjingwen12@users.noreply.github.com>
2023-03-24 10:29:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oSJnb56EblfkUWf_YPNkMa9xUV38',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	jsnQh9Y8lpI2NMVIkwIxD3YL-vUTqRQDd522_0EaC3o',
+      useTemplateId: 'jsnQh9Y8lpI2NMVIkwIxD3YL-vUTqRQDd522_0EaC3o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
